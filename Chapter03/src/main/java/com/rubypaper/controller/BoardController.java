a099eac18f728057039393854c49445783ee9e87
devbelly <devbelly@naver.com>
2021-06-29 13:35:46 +0900

    서비스 레이어 사용하는 컨트롤러 테스트

--- Chapter03/src/main/java/com/rubypaper/controller/BoardController.java
+++ Chapter03/src/main/java/com/rubypaper/controller/BoardController.java
@@ -1,11 +1,13 @@
 package com.rubypaper.controller;
 
-import java.util.Date;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rubypaper.domain.BoardVO;
+import com.rubypaper.service.BoardService;
 
 @RestController
 public class BoardController {
@@ -13,20 +15,21 @@ public class BoardController {
 		System.out.println("==> BoardController 생성");
 	}
 	
+	@Autowired
+	private BoardService boardService;
+	
 	@GetMapping("/hello")
 	public String hello(String name) {
-		return "hello:"+name;
+		return boardService.hello(name);
 	}
 	
 	@GetMapping("/getBoard")
 	public BoardVO getBoard() {
-		BoardVO board = new BoardVO();
-		board.setSeq(1);
-		board.setTitle("테스트 제목");
-		board.setWriter("테스터");
-		board.setContent("테스트 내용");
-		board.setCreateDate(new Date());
-		board.setCnt(0);
-		return board;
+		return boardService.getBoard();
+	}
+	
+	@GetMapping("/getBoardList")
+	public List<BoardVO> getBoardList(){
+		return boardService.getBoardList();
 	}
 }
--- Chapter03/src/test/java/com/rubypaper/BoardControllerTest.java
+++ Chapter03/src/test/java/com/rubypaper/BoardControllerTest.java
@@ -1,32 +1,25 @@
 package com.rubypaper;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=WebEnvironment.MOCK)
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class BoardControllerTest {
 	
 	@Autowired
-	private MockMvc mockMvc;
+	private TestRestTemplate restTemplate;
 	
 	@Test
 	public void testHello() throws Exception{
-		mockMvc.perform(get("/hello").param("name","둘리"))
-		.andExpect(status().isOk())
-		.andExpect(content().string("hello:둘리"))
-		.andDo(print());
+		String result = restTemplate.getForObject("/hello?name=둘리",String.class);
+		assertEquals("hello:둘리",result);
 	}
 }
