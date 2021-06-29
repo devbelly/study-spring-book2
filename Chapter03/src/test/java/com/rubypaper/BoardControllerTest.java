641af7df08b1c1af5fe686ff1a146c0c2f21abd0
devbelly <devbelly@naver.com>
2021-06-29 11:17:18 +0900

    @AutoConfigureMockMvc 를 통한 테스트케이스

--- Chapter03/src/test/java/com/rubypaper/BoardControllerTest.java
+++ Chapter03/src/test/java/com/rubypaper/BoardControllerTest.java
@@ -8,12 +8,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest(webEnvironment=WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class BoardControllerTest {
 	
 	@Autowired
