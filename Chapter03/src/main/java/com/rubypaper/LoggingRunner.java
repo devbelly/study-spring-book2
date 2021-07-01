fc4b54041793da25b0e812d5bc75ed08104e72e6
devbelly <devbelly@naver.com>
2021-07-01 10:00:30 +0900

    LoggingRunner 클래스

---
+++ Chapter03/src/main/java/com/rubypaper/LoggingRunner.java
@@ -0,0 +1,25 @@
+package com.rubypaper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoggingRunner implements ApplicationRunner {
+	private Logger logger = LoggerFactory.getLogger(LoggingRunner.class);
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+		logger.trace("Trace 로그 메세지");
+		logger.debug("Debug 로그 메세지");
+		logger.info("Info 로그 메세지");
+		logger.warn("Warn 로그 메세지");
+		logger.error("Error 로그 메세지");
+		
+	}
+	
+	
+}
