daed525a57c806b328f1877b9c708feb65cd2848
devbelly <devbelly@naver.com>
2021-07-02 15:52:14 +0900

    transaction을 통한 데이터 삽입

--- Chapter04/src/main/java/com/rubypaper/JPAClient.java
+++ Chapter04/src/main/java/com/rubypaper/JPAClient.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import com.rubypaper.domain.Board;
@@ -13,7 +14,10 @@ public class JPAClient {
 		EntityManagerFactory emf=Persistence.createEntityManagerFactory("Chapter04");
 		EntityManager em = emf.createEntityManager();
 		
+		EntityTransaction tx = em.getTransaction();
 		try {
+			tx.begin();
+			
 			Board board=new Board();
 			board.setTitle("JPA 제목");
 			board.setWriter("관리자");
@@ -22,8 +26,10 @@ public class JPAClient {
 			board.setCnt(0L);
 			
 			em.persist(board);
+			tx.commit();
 		}catch(Exception e) {
 			e.printStackTrace();
+			tx.rollback();
 		}finally {
 			em.close();
 			emf.close();
