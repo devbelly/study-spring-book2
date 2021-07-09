1976fab62a9ea8520639180645b27d199e191411
devbelly <devbelly@naver.com>
2021-07-09 11:33:50 +0900

    findByContentContaining 추가

--- Chapter05/src/test/java/com/rubypaper/QueryMethodTest.java
+++ Chapter05/src/test/java/com/rubypaper/QueryMethodTest.java
@@ -1,9 +1,7 @@
 package com.rubypaper;
 
-import java.util.Date;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,27 +17,34 @@ public class QueryMethodTest {
 	@Autowired
 	private BoardRepository boardRepo;
 	
-	@Before
-	public void dataPrepare() {
-		for(int i=1;i<=200;++i) {
-			Board board = new Board();
-			board.setTitle("테스트 제목 " + i);
-			board.setWriter("테스트");
-			board.setContent("테스트 내용" +i);
-			board.setCreateDate(new Date());
-			board.setCnt(0L);
-			boardRepo.save(board);
-		}
-	}
+//	@Before
+//	public void dataPrepare() {
+//		for(int i=1;i<=200;++i) {
+//			Board board = new Board();
+//			board.setTitle("테스트 제목 " + i);
+//			board.setWriter("테스트");
+//			board.setContent("테스트 내용" +i);
+//			board.setCreateDate(new Date());
+//			board.setCnt(0L);
+//			boardRepo.save(board);
+//		}
+//	}
+//	
+//	@Test
+//	public void testFindByTitle() {
+//		List<Board> boardList =boardRepo.findBytitle("테스트 제목 10");
+//		
+//		System.out.println("검색결과");
+//		for(Board board: boardList) {
+//			System.out.println(board);
+//		}
+//	}
 	
 	@Test
-	public void testFindByTitle() {
-		List<Board> boardList =boardRepo.findBytitle("테스트 제목 10");
-		
-		System.out.println("검색결과");
-		for(Board board: boardList) {
+	public void testFindByContentContaining() {
+		List<Board> boardList = boardRepo.findByContentContaining("17");
+		for(Board board : boardList) {
 			System.out.println(board);
 		}
 	}
-
 }
